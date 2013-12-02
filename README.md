2393d1dd5b77bff1f01e4cd323fffcb0bea35320
Matt Bernhardt <mjbernha@mit.edu>
2013-12-02 14:48:45 -0500

    Removing main menu Changing documentation around theme name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Chomsky-Archives
+unBox
 ================
 
-A WordPress theme for the "unBox the Chomsky Archives" website
+This is a stripped down WordPress theme based on the Twenty Twelve theme. It was designed and coded for a project at the MIT Libraries for work processing and digitizing the archives of Noam Chomsky.
--- header.php
+++ header.php
@@ -48,7 +48,6 @@
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
 			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
 
 	</header><!-- #masthead -->
