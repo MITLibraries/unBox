ea165d53cf32279660ec17183486aacaaa4619d7
Matt Bernhardt <mjbernha@mit.edu>
2013-12-02 14:49:36 -0500

    Adding functions and page template files

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * unBox functions and definitions.
+ *
+ * @package WordPress
+ * @subpackage unBox
+ * @since unBox 1.0
+ */
+
+add_filter( 'put_trailing_linebreak', '__return_false' );
