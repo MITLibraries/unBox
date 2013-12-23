6dfe6d2bc18a1af9749300b1f20eba8367840528
Matt Bernhardt <mjbernha@mit.edu>
2013-12-23 12:36:14 -0500

    Removing deprecated hgroup element

--- header.php
+++ header.php
@@ -34,10 +34,6 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<header id="masthead" class="site-header" role="banner">
-		<hgroup>
-			<!-- original h1
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			-->
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">unBox<br>the <em>Chomsky Archive</em></a></h1>
 			<?php $header_image = get_header_image();
 			if ( ! empty( $header_image ) ) : ?>
@@ -48,7 +44,6 @@
 				<?php dynamic_sidebar( 'masthead' ); ?>
 			</div>
 			<?php endif; ?>			
-		</hgroup>
 
 		<!-- Site navigation has been removed -->
 
