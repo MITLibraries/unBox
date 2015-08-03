83a9edb06650762cf4229563fd09c9a0dbb8409f
pbruk <phillip.bruk@gmail.com>
2015-08-03 13:05:46 -0400

    Remove absolute URL

--- footer.php
+++ footer.php
@@ -13,9 +13,9 @@
 	</div><!-- #main .wrapper -->
 	<footer id="colophon" role="contentinfo">
 		<div id="footerHeader" class="footerHeader">
-			<a href="http://libraries.mit.edu/" id="logoFooter">MIT Libraries</a>
+			<a href="//libraries.mit.edu/" id="logoFooter">MIT Libraries</a>
 			<div id="footerMainLink" class="">
-				<a href="http://libraries.mit.edu/archives/">Institute Archives</a> | 
+				<a href="//libraries.mit.edu/archives/">Institute Archives</a> | 
 				<a href="https://giving.mit.edu/givenow/add-designation.dyn?designationId=3843690">Give now <span class="hidden-phone">to the Chomsky archive</span></a>
 			</div>
 			<div id="socialFooter">
