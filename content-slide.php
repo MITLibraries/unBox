cba137584cf8f328ab285a745d89091b4a818754
Matt Bernhardt <mjbernha@mit.edu>
2015-10-08 10:47:16 -0400

    Starting code cleanup

--- content-slide.php
+++ content-slide.php
@@ -6,26 +6,26 @@
 	$caption = get_field("caption");
 
 	// Build layout style based on what images are present
-	$strLayout = "layout";
-	if($image_3) {
-		$strLayout .= "3";
-	} elseif($image_2) {
-		$strLayout .= "2";
+	$strLayout = 'layout';
+	if ( $image_3 ) {
+		$strLayout .= '3';
+	} elseif ( $image_2 ) {
+		$strLayout .= '2';
 	} else {
-		$strLayout .= "1";
+		$strLayout .= '1';
 	}
-	if($image['width']>$image['height']) {
-		$strLayout .= "h";
+	if ( $image['width'] > $image['height'] ) {
+		$strLayout .= 'h';
 	} else {
-		$strLayout .= "v";
+		$strLayout .= 'v';
 	}
 ?>
-<div class="cycleslide <?php echo $strLayout; ?> slide<?php echo $id; ?>" title="<?php echo the_title(); ?>">
+<div class="cycleslide <?php echo esc_attr( $strLayout ); ?> slide<?php echo esc_attr( $id ); ?>" title="<?php echo the_title(); ?>">
 	<div class="cycleslide-inner">
 		<div class="images">
 			<?php echo get_image_tag($image['id'],$image['alt'],$image['title'],"none","none"); ?>
-			<?php if($image_2) { echo get_image_tag($image_2['id'],$image_2['alt'],$image_2['title'],"none","none"); } ?>
-			<?php if($image_3) { echo get_image_tag($image_3['id'],$image_3['alt'],$image_3['title'],"none","none"); } ?>
+			<?php if ( $image_2 ) { echo get_image_tag( $image_2['id'],$image_2['alt'],$image_2['title'],"none","none" ); } ?>
+			<?php if ( $image_3 ) { echo get_image_tag( $image_3['id'],$image_3['alt'],$image_3['title'],"none","none" ); } ?>
 		</div>
 		<div class="caption"><div class="caption-inner"><?php echo $caption; ?></div></div>
 	</div>
