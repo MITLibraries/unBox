3c28c596d5c1de5ac099beec292de29940afb690
Matt Bernhardt <mjbernha@mit.edu>
2016-03-15 15:26:36 -0400

    Replaces spaces with tabs in javascript

--- page-slideshow.php
+++ page-slideshow.php
@@ -81,143 +81,143 @@ get_header(); ?>
 
 <script>
 jQuery(document).ready(function() {	
-    var theA, theB, theC, theD;
-    var x = jQuery.bbq.getState();
-    if (x.slide === undefined) {
-        theA = 0;
-        theB = 0;
-        theC = 0;
-        theD = 0;
-    } else {
-        var theSlide = x.slide;
-        var theTempShow = theSlide.substring(0, 1);
-        var theTempSlide = theSlide.substring(1, 2);
-        if (theTempShow == "a") {
-            theA = theTempSlide;
-        } else if (theTempShow == "b") {
-            theB = theTempSlide;
-        } else if (theTempShow == "c") {
-            theC = theTempSlide;
-        } else if (theTempShow == "d") {
-            theD = theTempSlide;
-        }
-    }
-    var intFold = parseInt(x.active,10);
+	var theA, theB, theC, theD;
+	var x = jQuery.bbq.getState();
+	if (x.slide === undefined) {
+		theA = 0;
+		theB = 0;
+		theC = 0;
+		theD = 0;
+	} else {
+		var theSlide = x.slide;
+		var theTempShow = theSlide.substring(0, 1);
+		var theTempSlide = theSlide.substring(1, 2);
+		if (theTempShow == "a") {
+			theA = theTempSlide;
+		} else if (theTempShow == "b") {
+			theB = theTempSlide;
+		} else if (theTempShow == "c") {
+			theC = theTempSlide;
+		} else if (theTempShow == "d") {
+			theD = theTempSlide;
+		}
+	}
+	var intFold = parseInt(x.active,10);
 
-    jQuery("#accordion").accordion({
-        active: intFold,
-        heightStyle: "content",
-        activate: function (event, ui) {
-            var active = jQuery("#accordion").accordion('option', 'active');
-            jQuery.bbq.pushState({'active': active}, 0);
-            if (active === 0) {
-               theSlide = 'a' + jQuery("#mit").data("cycle.opts").currSlide;
-            } else if (active == 1) {
-                theSlide = 'b' + jQuery("#linguistics").data("cycle.opts").currSlide;
-            } else if (active == 2) {
-                theSlide = 'c' + jQuery("#activism").data("cycle.opts").currSlide;
-            } else if (active == 2) {
-                theSlide = 'd' + jQuery("#political-writings").data("cycle.opts").currSlide;
-            }
-            jQuery.bbq.pushState({'slide': theSlide}, 0);
-        }
-    });
+	jQuery("#accordion").accordion({
+		active: intFold,
+		heightStyle: "content",
+		activate: function (event, ui) {
+			var active = jQuery("#accordion").accordion('option', 'active');
+			jQuery.bbq.pushState({'active': active}, 0);
+			if (active === 0) {
+				theSlide = 'a' + jQuery("#mit").data("cycle.opts").currSlide;
+			} else if (active == 1) {
+				theSlide = 'b' + jQuery("#linguistics").data("cycle.opts").currSlide;
+			} else if (active == 2) {
+				theSlide = 'c' + jQuery("#activism").data("cycle.opts").currSlide;
+			} else if (active == 2) {
+				theSlide = 'd' + jQuery("#political-writings").data("cycle.opts").currSlide;
+			}
+			jQuery.bbq.pushState({'slide': theSlide}, 0);
+		}
+	});
 
-   jQuery("#mit").cycle({
-       allowWrap: false,
-       timeout: 0,
-       slides: '> div.cycleslide',
-       startingSlide: theA,
-       autoHeight: 'container',
-       swipe: true
-   });
+	jQuery("#mit").cycle({
+		allowWrap: false,
+		timeout: 0,
+		slides: '> div.cycleslide',
+		startingSlide: theA,
+		autoHeight: 'container',
+		swipe: true
+	});
 
-   jQuery("#linguistics").cycle({
-       allowWrap: false,
-       timeout: 0,
-       slides: '> div.cycleslide',
-       startingSlide: theB,
-       autoHeight: 'container',
-       swipe: true
-   });
+	jQuery("#linguistics").cycle({
+		allowWrap: false,
+		timeout: 0,
+		slides: '> div.cycleslide',
+		startingSlide: theB,
+		autoHeight: 'container',
+		swipe: true
+	});
 
-   jQuery("#activism").cycle({
-       allowWrap: false,
-       timeout: 0,
-       slides: '> div.cycleslide',
-       startingSlide: theC,
-       autoHeight: 'container',
-       swipe: true
-   });
+	jQuery("#activism").cycle({
+		allowWrap: false,
+		timeout: 0,
+		slides: '> div.cycleslide',
+		startingSlide: theC,
+		autoHeight: 'container',
+		swipe: true
+	});
 
-   jQuery("#political-writings").cycle({
-       allowWrap: false,
-       timeout: 0,
-       slides: '> div.cycleslide',
-       startingSlide: theD,
-       autoHeight: 'container',
-       swipe: true
-   });
+	jQuery("#political-writings").cycle({
+		allowWrap: false,
+		timeout: 0,
+		slides: '> div.cycleslide',
+		startingSlide: theD,
+		autoHeight: 'container',
+		swipe: true
+	});
 
-    jQuery(window).bind('hashchange', function () {
-        var x = jQuery.bbq.getState();
-        var a = jQuery("#mit").data("cycle.opts").currSlide;
-        var b = jQuery("#linguistics").data("cycle.opts").currSlide;
-        var c = jQuery("#activism").data("cycle.opts").currSlide;
-        var d = jQuery("#political-writings").data("cycle.opts").currSlide;
-        if (x.active === undefined) {
-            jQuery("#accordion").accordion('option', 'active', 0);
-        } else {
-            jQuery("#accordion").accordion('option', 'active', x.active);
-        }
-    });
+	jQuery(window).bind('hashchange', function () {
+		var x = jQuery.bbq.getState();
+		var a = jQuery("#mit").data("cycle.opts").currSlide;
+		var b = jQuery("#linguistics").data("cycle.opts").currSlide;
+		var c = jQuery("#activism").data("cycle.opts").currSlide;
+		var d = jQuery("#political-writings").data("cycle.opts").currSlide;
+		if (x.active === undefined) {
+			jQuery("#accordion").accordion('option', 'active', 0);
+		} else {
+			jQuery("#accordion").accordion('option', 'active', x.active);
+		}
+	});
 
-    // Since the event is only triggered when the hash changes, we need to trigger
-    // the event now, to handle the hash the page may have loaded with.
-    jQuery(window).trigger('hashchange');
+	// Since the event is only triggered when the hash changes, we need to trigger
+	// the event now, to handle the hash the page may have loaded with.
+	jQuery(window).trigger('hashchange');
 
-    jQuery(".control a").click(function () {
-        // what was clicked on
-        var theControl = jQuery(this).attr('class');
-        var theShow = jQuery(this).parents("ul.control").attr('data-show').toLowerCase();
-        var theThing = document.getElementById(theShow);
-        // get the relevant show's current slide
-        if (theControl == 'next') {
-           jQuery(theThing).cycle('next');
-        } else if (theControl == 'prev') {
-           jQuery(theThing).cycle('prev');
-        }
-        var newSlide = jQuery(theThing).data("cycle.opts").currSlide+1;
-        var theLength = jQuery(theThing).data("cycle.opts").slideCount;
-        if(newSlide == 1 || newSlide == theLength){
-          jQuery(this).addClass('hidden');
-        } else {
-          jQuery(".control a").removeClass('hidden');
-        }
-        if(theShow=='mit') {
-           var theSlide = 'a' + jQuery(theThing).data("cycle.opts").currSlide;
-        } else if (theShow == 'linguistics') {
-           var theSlide = 'b' + jQuery(theThing).data("cycle.opts").currSlide;
-        } else if (theShow == 'activism') {
-           var theSlide = 'c' + jQuery(theThing).data("cycle.opts").currSlide;
-        } else if (theShow == 'political-writings') {
-           var theSlide = 'd' + jQuery(theThing).data("cycle.opts").currSlide;
-        }
-        jQuery.bbq.pushState({'slide': theSlide}, 0);
-   });
+	jQuery(".control a").click(function () {
+		// what was clicked on
+		var theControl = jQuery(this).attr('class');
+		var theShow = jQuery(this).parents("ul.control").attr('data-show').toLowerCase();
+		var theThing = document.getElementById(theShow);
+		// get the relevant show's current slide
+		if (theControl == 'next') {
+			jQuery(theThing).cycle('next');
+		} else if (theControl == 'prev') {
+			jQuery(theThing).cycle('prev');
+		}
+		var newSlide = jQuery(theThing).data("cycle.opts").currSlide+1;
+		var theLength = jQuery(theThing).data("cycle.opts").slideCount;
+		if(newSlide == 1 || newSlide == theLength){
+			jQuery(this).addClass('hidden');
+		} else {
+			jQuery(".control a").removeClass('hidden');
+		}
+		if(theShow=='mit') {
+			var theSlide = 'a' + jQuery(theThing).data("cycle.opts").currSlide;
+		} else if (theShow == 'linguistics') {
+			var theSlide = 'b' + jQuery(theThing).data("cycle.opts").currSlide;
+		} else if (theShow == 'activism') {
+			var theSlide = 'c' + jQuery(theThing).data("cycle.opts").currSlide;
+		} else if (theShow == 'political-writings') {
+			var theSlide = 'd' + jQuery(theThing).data("cycle.opts").currSlide;
+		}
+		jQuery.bbq.pushState({'slide': theSlide}, 0);
+	});
 
-  // Remote control of accordion via a.remote links (inline in slides)
-  jQuery("a.remote").click(function(e) {
-    var x = jQuery.bbq.getState();
-    var y = x.active;
-    if(y>=3) {
-      y = 0;
-    } else {
-      y++;
-    }
-    jQuery("#accordion").accordion('option','active',y);
-    e.preventDefault();
-  });
+	// Remote control of accordion via a.remote links (inline in slides)
+	jQuery("a.remote").click(function(e) {
+		var x = jQuery.bbq.getState();
+		var y = x.active;
+		if(y>=3) {
+			y = 0;
+		} else {
+			y++;
+		}
+		jQuery("#accordion").accordion('option','active',y);
+		e.preventDefault();
+	});
 
 });
 </script>
